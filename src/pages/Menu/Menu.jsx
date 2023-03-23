a83dc5a849c25e22d24b990ba81fa00ac971fc67
yuelin-wen <tyleryuelinwen@gmail.com>
2023-03-22 20:46:04 -0400

    update

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ItemList from '../../components/Menu/ItemList/ItemList';
 import MenuSummary from '../../components/Menu/MenuSummary/MenuSummary';
+import './menu.css'
 
 const Menu = () => {
     return (
-        <div>
+        <div className='menu' >
             <MenuSummary />
             <ItemList />
         </div>
